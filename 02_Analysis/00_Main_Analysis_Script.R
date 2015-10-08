4e1fde91c7c69acac0a1ad2b3b0f595217ace177
jaspercooper <jaspercooper@gmail.com>
2015-10-08 17:43:06 -0400

    Generated the main analysis script

---
+++ 02_Analysis/00_Main_Analysis_Script.R
@@ -0,0 +1,30 @@
+# Our awesome collaborative project ---------------------------------------
+
+
+
+# Run the code that generates our dummy data ------------------------------
+
+# Define breaks 
+
+income_breaks <- quantile(x = 0:1)
+
+source("02_Analysis/01_Generate_Fake_Data.R")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
